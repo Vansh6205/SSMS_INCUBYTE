c4454435130af42cc46efda92407ed463c7db311
VanshPatel <vanshpatel6205@gmail.com>
2025-07-17 10:00:47 +0530

    reqiure set up and sweet function

---
+++ src/app.js
@@ -0,0 +1,9 @@
+class Sweetshopmanagementsystem {
+    #availableSweet = [];
+
+    // Method to add a book to the library
+    addSweet(book) {
+        this.#availableSweet.push(sweet);
+        console.log(`Sweet${sweet.ISBN} added successfully!`);
+    }
+}
